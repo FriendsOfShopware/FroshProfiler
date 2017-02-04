f07e3785760e803bf004a5cb09259aaf39d961ff
Soner Sayakci <s.sayakci@gmail.com>
2017-02-04 16:14:53 +0100

    Moved Icons in own files

---
+++ Resources/smarty/compiler.fetchSvg.php
@@ -0,0 +1,13 @@
+<?php
+
+function smarty_compiler_fetchSvg($params, Smarty $smarty)
+{
+    $params['file'] = str_replace('"', '', $params['file']);
+    foreach ($smarty->getTemplateDir() as $folder) {
+        if (file_exists($folder . $params['file'])) {
+            return file_get_contents($folder . $params['file']);
+        }
+    }
+
+    return '';
+}
