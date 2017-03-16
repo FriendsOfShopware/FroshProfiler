88ef2c3dcd879f68807f251c9c58816b863dcb2f
Soner Sayakci <s.sayakci@gmail.com>
2017-03-16 16:31:56 +0100

    Added configuration for var dump depth #40

--- Resources/smarty/modifier.dump.php
+++ Resources/smarty/modifier.dump.php
@@ -7,6 +7,8 @@ function smarty_modifier_dump($var)
 {
     $dumper = new HtmlDumper();
     $cloner = new VarCloner();
+    $data = $cloner->cloneVar($var);
+    $data->withMaxDepth(Shopware()->Config()->get('dumpDepth', 20));
 
-    return $dumper->dump($cloner->cloneVar($var));
+    return $dumper->dump($data);
 }
