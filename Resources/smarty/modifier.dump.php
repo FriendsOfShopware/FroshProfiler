bc2d30204cdb71c5fd660bd314737899dac9b6e6
Fionera <fionera@fionera.de>
2018-03-14 12:27:14 +0100

    Set MaxItems in cloner to PHP_INT_MAX
    Fixes #66

--- Resources/smarty/modifier.dump.php
+++ Resources/smarty/modifier.dump.php
@@ -7,6 +7,7 @@ function smarty_modifier_dump($var)
 {
     $dumper = new HtmlDumper();
     $cloner = new VarCloner();
+    $cloner->setMaxItems(PHP_INT_MAX);
     $data = $cloner->cloneVar($var);
     $data->withMaxDepth(Shopware()->Config()->get('dumpDepth', 20));
 
