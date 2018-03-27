279fca29460bb9cae0f390734eecedfe1a8f4a68
Soner Sayakci <shyim@posteo.de>
2018-03-27 21:23:08 +0200

    Backported var_dump server from Symfony 4.1

---
+++ Components/VarDumper/RequestContextProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace FroshProfiler\Components\VarDumper;
+
+use Enlight_Controller_Front;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\ContextProviderInterface;
+
+/**
+ * Class RequestContextProvider
+ * @author Soner Sayakci <shyim@posteo.de>
+ */
+class RequestContextProvider implements ContextProviderInterface
+{
+    /**
+     * @var Enlight_Controller_Front
+     */
+    private $front;
+
+    /**
+     * RequestContextProvider constructor.
+     * @param Enlight_Controller_Front $front
+     * @author Soner Sayakci <shyim@posteo.de>
+     */
+    public function __construct(Enlight_Controller_Front $front)
+    {
+        $this->front = $front;
+    }
+
+    /**
+     * @return array|null Context data or null if unable to provide any context
+     */
+    public function getContext()
+    {
+        $request = $this->front->Request();
+
+        return array(
+            'uri' => $request->getRequestUri(),
+            'method' => $request->getMethod(),
+            'controller' => $request->getControllerName(),
+            'identifier' => spl_object_hash($request),
+        );
+    }
+}
---
+++ Subscriber/VarDumpServerSubscriber.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace FroshProfiler\Subscriber;
+
+
+use Enlight\Event\SubscriberInterface;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\ServerDumper;
+use Symfony\Component\VarDumper\VarDumper;
+
+/**
+ * Class VarDumpServerSubscriber
+ * @author Soner Sayakci <shyim@posteo.de>
+ */
+class VarDumpServerSubscriber implements SubscriberInterface
+{
+    /**
+     * @var array
+     */
+    private $pluginConfig;
+
+    /**
+     * @var ServerDumper
+     */
+    private $serverDumper;
+
+    /**
+     * VarDumpServerSubscriber constructor.
+     * @param array $pluginConfig
+     * @param ServerDumper $serverDumper
+     * @author Soner Sayakci <shyim@posteo.de>
+     */
+    public function __construct(array $pluginConfig, ServerDumper $serverDumper)
+    {
+        $this->pluginConfig = $pluginConfig;
+        $this->serverDumper = $serverDumper;
+    }
+
+    /**
+     * @return array
+     * @author Soner Sayakci <shyim@posteo.de>
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Bootstrap_AfterInitResource_models' => 'initVarDumper',
+        ];
+    }
+
+    /**
+     * @param \Enlight_Event_EventArgs $args
+     * @author Soner Sayakci <shyim@posteo.de>
+     */
+    public function initVarDumper(\Enlight_Event_EventArgs $args)
+    {
+        if (empty($this->pluginConfig['varDumpServer'])) {
+            return;
+        }
+
+        $cloner = new VarCloner();
+        $cloner->setMaxItems(-1);
+        $serverDumper = $this->serverDumper;
+
+        VarDumper::setHandler(function($var) use ($cloner, $serverDumper) {
+            $data = $cloner->cloneVar($var);
+            $serverDumper->dump($data);
+        });
+    }
+}
